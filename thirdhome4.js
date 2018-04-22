38f585000bc1ef96ea8c61b6688187675b4afd44
LusinAvaki <lusinavaki@gmail.com>
2018-04-23 03:47:12 +0400

    checking the equality of arrays

---
+++ thirdhome4.js
@@ -0,0 +1,9 @@
+function isEqual(arrA, arrB) {
+    for (let i = 0; i < arrA.length; i++) {
+        if (arrA[i] !== arrB[i]) return false;
+    }
+    return true;
+}
+let a = [1, 2, 3, 4, 5];
+let b = [1, 2, 3, 4, 5];
+console.log(isEqual(a, b));
