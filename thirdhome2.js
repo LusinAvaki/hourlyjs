faa0c3edea3348926881766603d29609d6e74d09
LusinAvaki <lusinavaki@gmail.com>
2018-04-25 14:00:23 +0400

    I haven't done this one yet, need to check

---
+++ thirdhome2.js
@@ -0,0 +1,16 @@
+function stringChanger(str) {
+    for (let i = 0; i < str.length; i++) {
+        str.split("");
+        str.toLowerCase();
+        let compareIndex = i;
+        let replaceIndex = 0;
+            if (compareIndex = replaceIndex) {
+                str.replace(compareIndex, '*');
+                 } else {
+            return stringChanger();
+        }
+    }
+}
+
+console.log(stringChanger("calculator"));
+// don't know(
