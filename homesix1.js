83e53eeabc4515ff8d0cdba7138a13437455ce2d
LusinAvaki <lusinavaki@gmail.com>
2018-05-11 18:25:27 +0400

    files returned in an array with promises

--- homesix1.js
+++ homesix1.js
@@ -4,26 +4,15 @@ function readFile(path, options) {
     return new Promise((resolve, reject) => {
         return fs.readFile(path, options, (err, data) => {
             if (err) {
-                return Promise.reject(err)
+                reject(err);
+            } else {
+                resolve(data);
             }
-            return Promise.resolve(data)
         })
     })
 }
 
-
-Promise.resolve()
-    .then(() => {
-        fs.readFile('./Users.json', 'utf8', function (err, contents) {
-            console.log(contents);
-        });
-    })
-    .then(() => {
-        fs.readFile('./Users.backup.json', 'utf8', function (err, contents) {
-            console.log(contents);
-        });
-    })
-    .catch(() => {
-        console.log('error');
-
-    });
+let p1 = readFile('./Users.json', 'utf8');
+let p2 = readFile('./Users.backup.json', 'utf8');
+Promise.all([p1, p2])
+    .then(data => console.log(data));
