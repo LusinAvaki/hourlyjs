8c1328acd983dab8b1ce111852b15ddbd6d58c27
LusinAvaki <lusinavaki@gmail.com>
2018-05-15 16:36:12 +0400

    promise all

--- homesix1-1.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-
-fs.readFile('./Users.json', 'utf8', function (err, contents) {
-    console.log(contents);
-    fs.copyFile('./Users.json', 'Users.backup.json', (err) => {
-        if (err) throw err;
-        console.log('source.txt was copied');
-    });
-});
--- homesix1.js
+++ homesix1.js
@@ -4,15 +4,34 @@ function readFile(path, options) {
     return new Promise((resolve, reject) => {
         return fs.readFile(path, options, (err, data) => {
             if (err) {
-                reject(err);
+                return reject(err);
             } else {
-                resolve(data);
+                return resolve(data);
             }
         })
     })
 }
 
-let p1 = readFile('./Users.json', 'utf8');
-let p2 = readFile('./Users.backup.json', 'utf8');
-Promise.all([p1, p2])
-    .then(data => console.log(data));
+function all(contents) {
+    let results = [];
+    let completedPromises = 0;
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < contents.length; i++) {
+            contents[i].then((data) => {
+                results[i] = data;
+                completedPromises += 1;
+            if (completedPromises === contents.length) {
+                return resolve(results);
+            }
+            }).catch(error => reject(error))
+        }
+    })
+}
+
+let p1 = readFile('./Users.json', 'utf8')
+let p2 = readFile('./Users.backup.json', 'utf8')
+
+
+let promiseToMake = [p1, p2];
+let contents = all(promiseToMake);
+contents.then(results=> console.log(results));
